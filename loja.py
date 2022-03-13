073223deec178e41e8de54465306a1e957d6a8b0
Lucas Gabriel Dias Borges <diasborges2@gmail.com>
2022-03-13 14:28:16 -0300

    Adicionando getters

--- loja.py
+++ loja.py
@@ -9,3 +9,15 @@ class Vendas:
         print(self._produto)
         print("Valor: ", self._valor)
         print("Quantidade: ", self._qtd)
+
+    @property
+    def produto(self):
+        return self._produto
+
+    @property
+    def valor(self):
+        return self._valor
+
+    @property
+    def qtd(self):
+        return self._qtd
